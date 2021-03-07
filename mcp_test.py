0ea8b7a34afe6d714e935044ee922bcfb07a810d
wjarchambault <wjarchambault@gmail.com>
2021-03-07 13:34:00 -0800

    Implement PWM for lids

--- mcp_test.py
+++ mcp_test.py
@@ -5,6 +5,7 @@ import random
 import busio
 import digitalio
 import board
+import pwmio
 import adafruit_mcp3xxx.mcp3008 as MCP
 from adafruit_mcp3xxx.analog_in import AnalogIn
 
@@ -20,8 +21,10 @@ reset.direction = digitalio.Direction.OUTPUT
 strobe = digitalio.DigitalInOut(board.D21)
 strobe.direction = digitalio.Direction.OUTPUT
 
-mEnable = digitalio.DigitalInOut(board.D13)
-mEnable.direction = digitalio.Direction.OUTPUT
+m1Enable = digitalio.DigitalInOut(board.D13)
+m1Enable.direction = digitalio.Direction.OUTPUT
+m2Enable = pwmio.PWMOut(board.D6)
+
 in1 = digitalio.DigitalInOut(board.D4)
 in1.direction = digitalio.Direction.OUTPUT
 in2 = digitalio.DigitalInOut(board.D17)
@@ -31,6 +34,19 @@ in3.direction = digitalio.Direction.OUTPUT
 in4 = digitalio.DigitalInOut(board.D26)
 in4.direction = digitalio.Direction.OUTPUT
 
+high_value = 33000
+low_value = 22000
+
+low_lids_value = 13500
+high_lids_value = 33500
+
+#PWM duty cycle choices
+duty_cycle_100 = 65535
+duty_cycle_75 = 49152
+duty_cycle_50 = 32768
+duty_cycle_25 = 16384
+duty_cycle_0 = 0
+
 # create the mcp object
 mcp = MCP.MCP3008(spi, cs)
 
@@ -38,25 +54,52 @@ mcp = MCP.MCP3008(spi, cs)
 #blockChar = u'\u2591'+u'\u2591'
 blockChar = u'\u2591' * 2
 
-def open_mouth():
-   in1.value = True
-   in2.value = False
+def open_mouth(mouth_value):
+   if mouth_value < high_value:
+#       print("Opening mouth:", mouth_value)
+       in1.value = True
+       in2.value = False
+   else:
+#       print("Open Mouth Limit reached", mouth_value)
+       in1.value = False
+       in2.value = False
 
-def close_mouth():
-   in1.value = False
-   in2.value = True
+def close_mouth(mouth_value):
+   if mouth_value > low_value+3000:
+#       print("Closing mouth:", mouth_value)
+       in1.value = False
+       in2.value = True
+   else:
+#       print("Close mouth Limit reached:", mouth_value)
+       in1.value = False
+       in2.value = False
 
 def relax_mouth():
    in1.value = False
    in2.value = False
 
-def open_lids():
-   in3.value = False
-   in4.value = True
+def open_lids(lids_value):
+   global random_blink
+   #print("Open lids", lids_value)
+   if lids_value < high_lids_value:
+       in3.value = False
+       in4.value = True
+   else:
+       m2Enable.duty_cycle = duty_cycle_0
+       random_blink = False
+       in3.value = False
+       in4.value = False
 
-def close_lids():
-   in3.value = True
-   in4.value = False
+def close_lids(lids_value):
+   global close_the_lids
+   #print("Close lids", lids_value)
+   if lids_value > low_lids_value:
+       in3.value = True
+       in4.value = False
+   else:
+       close_the_lids = False
+       in3.value = False
+       in4.value = False
 
 def relax_lids():
    in3.value = False
@@ -64,8 +107,8 @@ def relax_lids():
 
 # create analog input channels
 chan0 = AnalogIn(mcp, MCP.P0)
-
-#print("\nChan0", dir(chan0))
+chan1 = AnalogIn(mcp, MCP.P1)
+chan2 = AnalogIn(mcp, MCP.P2)
 
 # Init spectrum analyzer
 strobe.value = False
@@ -80,57 +123,49 @@ reset.value = False
 time.sleep(0.01)
 
 # Init motor driver
-mEnable.value = True
-try_count = 0
-no_sound = True
+m1Enable.value = True
+m2Enable.duty_cycle = duty_cycle_50
 
 try:
-    a = datetime.datetime.now()
-    random_num = random.randint(0,9)
-    lids_closed = False
+    close_the_lids = False
     random_blink = False
-    open_lids()    
-    time.sleep(1)
+    lids_value = chan2.value
+    while lids_value < high_lids_value:
+        lids_value = chan2.value
+        open_lids(lids_value)
+    in3.value = False
+    in4.value = False
+    m2Enable.duty_cycle = duty_cycle_0
+    blink_now = [random.randrange(1, 60, 1) for i in range(14)]
     while True:
-        b = datetime.datetime.now()
-        c = b - a
-        tens, ones = divmod(a.second, 10)
+        a = datetime.datetime.now()
         if not random_blink:
-           if ones == random_num:
-              close_lids()
-              random_blink = True
-              lids_closed = True
-        if c.microseconds > 455000:
-           a = datetime.datetime.now()
-           if lids_closed:
-              open_lids()
-              random_blink = False
-              lids_closed = False
-              random_num = random.randint(0,9)
-           else:
-              relax_lids()
-        try_count = try_count + 1
+            if a.second in blink_now:
+                m2Enable.duty_cycle = duty_cycle_50
+                random_blink = True
+                close_the_lids = True
+#                print("Random eye blink activated...", a.second)
+        lids_value = chan2.value
+        if close_the_lids:
+            close_lids(lids_value)
+        else:
+            open_lids(lids_value)
         chanLeft = []
         for freq in range(4):
             strobe.value = True
             strobe.value = False
             chanLeft.append(int(chan0.voltage))
-        sumLeft = sum(chanLeft)
+#        sumLeft = sum(chanLeft)
+        sumLeft = chanLeft[0]+chanLeft[2]+chanLeft[3]
+        mouth_value = chan1.value
         if sumLeft > 0:
 #            print(blockChar * sumLeft)
-            no_sound = False
-            open_mouth()
+            open_mouth(mouth_value)
         else:
-            if no_sound:
-               relax_mouth()
-            else:
-               close_mouth()
-#            print("\n")
-        if try_count > 250:
-            no_sound = True
-            try_count=0
+            close_mouth(mouth_value)
 except KeyboardInterrupt:
     print('Keyboard Interrupt')
-    mEnable.value = False
+    m1Enable.value = False
+    m2Enable.value = False
     relax_mouth()
     relax_lids()
